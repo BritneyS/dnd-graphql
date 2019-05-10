3a4f70fdf04914b3e392e0a4789c706afb3acecc
Britney Smith <brit.smith7@gmail.com>
2019-05-10 10:11:17 -0400

    Express setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+const ENV_PORT = process.env.ENV_PORT;
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(ENV_PORT, () =>
+  console.log(`Example app listening on port ${ENV_PORT}`),
+);
 
 console.log('Hello Node.js project.');
-const ENV_PORT = process.env.ENV_PORT;
