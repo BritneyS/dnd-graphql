3c8f6d3e5d933d1aeae8a6c976e89a5b339a2586
Britney Smith <brit.smith7@gmail.com>
2019-05-10 13:58:53 -0400

    Query with sample data works as expected

--- src/index.js
+++ src/index.js
@@ -2,24 +2,43 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import fetch from 'node-fetch';
 
 const app = express();
 const ENV_PORT = process.env.ENV_PORT;
+const classesEndpoint = 'http://dnd5eapi.co/api/classes/';
 
 app.use(cors());
 const schema = gql`
     type Query {
-        foo: User
+        result: Result
+        class: Class
+        results: [Class]
     }
 
-    type User {
-        name: String!
+    type Result {
+        count: Int
+        results: [Class]
+    }
+
+    type Class {
+        name: String
     }
 `;
 const resolvers = {
     Query: {
-        foo: () => {
-            return { name: 'Britney Smith' };
+        result: () => {
+            //return { name: 'Britney Smith' };
+            //return fetch(classesEndpoint).then(res => res.json());
+            return Object.values(data);
+        },
+    },
+    Result: {
+        count: (parent) => {
+            return data.count;
+        },
+        results: () => {
+            return Object.values(data.results);
         },
     },
 };
@@ -36,3 +55,122 @@ app.listen({ port: ENV_PORT }, () => {
 });
 
 console.log('Hello Node.js project.');
+
+
+let data = {
+    "count": 12,
+    "results": [
+        {
+            "name": "Barbarian",
+            "url": "http://www.dnd5eapi.co/api/classes/1"
+        },
+        {
+            "name": "Bard",
+            "url": "http://www.dnd5eapi.co/api/classes/2"
+        },
+        {
+            "name": "Cleric",
+            "url": "http://www.dnd5eapi.co/api/classes/3"
+        },
+        {
+            "name": "Druid",
+            "url": "http://www.dnd5eapi.co/api/classes/4"
+        },
+        {
+            "name": "Fighter",
+            "url": "http://www.dnd5eapi.co/api/classes/5"
+        },
+        {
+            "name": "Monk",
+            "url": "http://www.dnd5eapi.co/api/classes/6"
+        },
+        {
+            "name": "Paladin",
+            "url": "http://www.dnd5eapi.co/api/classes/7"
+        },
+        {
+            "name": "Ranger",
+            "url": "http://www.dnd5eapi.co/api/classes/8"
+        },
+        {
+            "name": "Rogue",
+            "url": "http://www.dnd5eapi.co/api/classes/9"
+        },
+        {
+            "name": "Sorcerer",
+            "url": "http://www.dnd5eapi.co/api/classes/10"
+        },
+        {
+            "name": "Warlock",
+            "url": "http://www.dnd5eapi.co/api/classes/11"
+        },
+        {
+            "name": "Wizard",
+            "url": "http://www.dnd5eapi.co/api/classes/12"
+        }
+    ]
+};
+
+/*
+
+Query:
+
+{
+  result {
+   count
+   results {
+   		name
+	  }
+  }
+}
+
+
+Result:
+
+{
+  "data": {
+    "result": {
+      "count": 12,
+      "results": [
+        {
+          "name": "Barbarian"
+        },
+        {
+          "name": "Bard"
+        },
+        {
+          "name": "Cleric"
+        },
+        {
+          "name": "Druid"
+        },
+        {
+          "name": "Fighter"
+        },
+        {
+          "name": "Monk"
+        },
+        {
+          "name": "Paladin"
+        },
+        {
+          "name": "Ranger"
+        },
+        {
+          "name": "Rogue"
+        },
+        {
+          "name": "Sorcerer"
+        },
+        {
+          "name": "Warlock"
+        },
+        {
+          "name": "Wizard"
+        }
+      ]
+    }
+  }
+}
+
+*/
