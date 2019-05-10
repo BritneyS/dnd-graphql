1e3802e3c66e023887858bea1d34762a6d692be3
Britney Smith <brit.smith7@gmail.com>
2019-05-10 10:53:36 -0400

    Apollo and GraphQL setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express();
 const ENV_PORT = process.env.ENV_PORT;
 
 app.use(cors());
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const schema = gql`
+    type Query {
+        foo: User
+    }
+
+    type User {
+        name: String!
+    }
+`;
+const resolvers = {
+    Query: {
+        foo: () => {
+            return { name: 'Britney Smith' };
+        },
+    },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
 });
-app.listen(ENV_PORT, () =>
-  console.log(`Example app listening on port ${ENV_PORT}`),
-);
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: ENV_PORT }, () => {
+  console.log(`Apollo Server on http://localhost:${ENV_PORT}/graphql`);
+});
 
 console.log('Hello Node.js project.');
